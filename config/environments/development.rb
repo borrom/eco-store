d1bdb7880d9a3db34661abe87f977235458d7aff
OSP <orsereiparinha@gmail.com>
2016-03-22 10:45:45 +0700

    Passed -- Notify User

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,16 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address    => "smtp.gmail.com",
+      :port       => 587,
+      :domain     => "gmail.com",
+      :authentication => "plain",
+      :user_name  => "orsereiparinha@gmail.com",
+      :password   => "Jqj7p1s5",
+      :enable_starttls_auto => true
+  }
 end
